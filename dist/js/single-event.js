e5770dc1f0d34b8508b913118093fda0ffa77012
Elvis Saavedra <esaavedrabs@belatrixsf.com>
2017-10-24 11:35:16 -0500

    fixed bugs

---
+++ dist/js/single-event.js
@@ -0,0 +1,1 @@
+$(function(){$(".navbar-custom").addClass("navbar-dark"),$(".navbar-custom li:nth-child(5)").addClass("current-menu-item")});
---
+++ js/single-event.js
@@ -0,0 +1,4 @@
+$(function(){
+	$('.navbar-custom').addClass('navbar-dark');
+	$('.navbar-custom li:nth-child(5)').addClass('current-menu-item');
+});
